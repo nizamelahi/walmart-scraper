aac4cd62654fd4466f6b205ee5543d7713be9c14
suleman ashfaq <suleman.ashfaq@compliance.ai>
2023-07-10 17:00:21 +0500

    minor

--- script.py
+++ script.py
@@ -2,10 +2,7 @@ from bs4 import BeautifulSoup
 import json
 import os
 import hashlib
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 import undetected_chromedriver as uc
 import time
 
